46ae5b440ccbd623db652775a9ee8b96221916fe
aretirenuka <84584679+aretirenuka@users.noreply.github.com>
2021-11-29 22:34:50 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{
+      int a,b,sum;
+      cout<<"enter a,b values";
+      cin>>a>>b;
+      sum=a+b;
+      cout<<"sum is"<<sum;
+      }
+      
