9760d2e4c75eb38cb35b306ff56bfd155e977f51
aretirenuka <84584679+aretirenuka@users.noreply.github.com>
2022-01-14 13:55:42 +0530

    Add files via upload

---
+++ ar.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    FILE *fp;
+    char *str;
+    fp=fopen("demo.txt","r");// demo.txt :you are a good programmer
+    while(fgets(str,6,fp)!=NULL)
+    puts(str);
+    fclose(fp);
+    return 0;
+}
